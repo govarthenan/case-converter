76ea7656b89d37050c7683fb3d8b909bdf13b3c3
Govarthenan Rajadurai <govarthenan@gmail.com>
2022-04-01 17:48:32 +0530

    Add comments

--- converter.py
+++ converter.py
@@ -2,6 +2,7 @@ string = input("Paste input here: ").strip()
 
 words = string.split(" ")  # breaking the string into individual words
 
+# changing all words are into lowercase
 for index, word in enumerate(words):
     words[index] = word.lower()
 
